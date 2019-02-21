ef9fb86cf917e1710d86a853de5def5edba97e00
skyer <long2805l2@gmail.com>
2019-02-21 20:20:20 +0700

    -add router

--- client/routes.jsx
+++ client/routes.jsx
@@ -6,11 +6,12 @@ import App from '/imports/ui/App';
 import Home from '/imports/ui/pages/Home.jsx';
 import Viewer from '/imports/ui/pages/Viewer.jsx';
 import Uploader from '/imports/ui/pages/Uploader.jsx';
+import Comic from '/imports/ui/pages/Comic.jsx';
 import ComicAdd from '/imports/ui/pages/ComicAdd.jsx';
 
 FlowRouter.route('/', {
 	name: 'Home',
-	action(){
+	action(params, queryParams){
 		mount( App, {
 			content: <Home />
 		})
@@ -18,18 +19,28 @@ FlowRouter.route('/', {
 });
 
 FlowRouter.route('/comic/add', {
-	name: 'Viewer',
-	action(){
+	name: 'comic.add',
+	action(params, queryParams){
 		mount( App, {
 			content: <ComicAdd />
 		})
 	}
 });
 
+FlowRouter.route('/comic/:comic_id', {
+	name: 'comic.info',
+	action(params, queryParams){
+		console.log("comic.info", params, queryParams);
+		mount( App, {
+			content: <Comic />
+		})
+	}
+});
 
-FlowRouter.route('/comic', {
-	name: 'Viewer',
-	action(){
+FlowRouter.route('/comic/:comic_id/:comic_chapter_id', {
+	name: 'comic.viewer',
+	action(params, queryParams){
+		console.log("comic.viewer", params, queryParams);
 		mount( App, {
 			content: <Viewer />
 		})
